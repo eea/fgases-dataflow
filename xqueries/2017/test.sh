38b21675c01fe897b699d183dad780b151b67c7b
laszlocseh <csehlaszlo91@gmail.com>
2017-12-22 14:49:12 +0200

    added some more data in test xml

--- xqueries/2017/test.sh
+++ xqueries/2017/test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-basex/bin/basex -bsource_url=fgases-test.xml fgases-2017.xquery > out.html && google-chrome-stable out.html
+basex/bin/basex -bsource_url=tests/qc-2017-test.xml fgases-2017.xquery > out.html && google-chrome-stable out.html
